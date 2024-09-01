3f09e75d08cc1f26c423f3e0c019ec16ac9cc7b0
Dmitry_Bezgubov <dmitrij-bezgubov@yandex.ru>
2024-09-02 00:54:33 +0500

    Two woking code почти exelent

--- src/processing.py
+++ src/processing.py
@@ -1,17 +1,20 @@
+from datetime import datetime
 
-def filter_by_state(list_dicts):
+
+def filter_by_state(operations, state='EXECUTED'):
+    '''Фильтрация операций по состоянию'''
+    filtered_operations = [i for i in operations if i['state'] == state]
+
+    return filtered_operations
+
+def sort_by_date(filtered_operations, reverse=True):
+    ''' Сортировка операций по дате'''
+    sorted_list = sorted(filtered_operations, key=lambda x: datetime.strptime(x['date'], '%Y-%m-%dT%H:%M:%S.%f'), reverse=reverse)
+
+    return sorted_list
 
 
-    list_key_stat = []
-    state = 'EXECUTED'
 
-    for i in list_dicts:
-        if i['state'] == state:
-            list_key_stat.append(i)
-        else:
-            list_dicts.remove(i)
-
-    return list_key_stat
 print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
 
 
