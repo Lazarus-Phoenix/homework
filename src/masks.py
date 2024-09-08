ddbf30fb0c1f10847ba8d390412dde13d10d3762
Dmitry_Bezgubov <dmitrij-bezgubov@yandex.ru>
2024-09-08 21:02:07 +0500

    Made corrections

--- src/masks.py
+++ src/masks.py
@@ -2,6 +2,8 @@ def get_mask_card_number(string_number: str) -> str:
     """принимает на вход номер карты в виде числа и возвращает
     маску номера по правилу XXXX XX** **** XXXX"""
     number_card = str(string_number)
+    if number_card == "":
+        return "Введите корректный номер"
 
     return f"{number_card[0:4]} {number_card[4:6]}** **** {number_card[12:16]}"
 
@@ -16,8 +18,8 @@ def get_mask_account(account_list: str) -> str:
     return viveport
 
 
-card = str(get_mask_card_number(7000792289606361))
-account = str(get_mask_account(73654108430135874305))
+card = str(get_mask_card_number('7000792289606361'))
+account = str(get_mask_account('73654108430135874305'))
 
 if __name__ == "__main__":
     print(card)
