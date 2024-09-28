47d1969470d62721647a7fb57f0b372c8f9475c2
Dmitry_Bezgubov <dmitrij-bezgubov@yandex.ru>
2024-09-28 19:12:58 +0500

    Created JSON file & utils.py module

---
+++ src/utils.py
@@ -0,0 +1,28 @@
+import json
+import os
+
+
+def read_transactions_from_json():
+    """
+    Читает JSON-файл из директории data в корне проекта и возвращает список словарей с данными о финансовых транзакциях.
+
+    Returns:
+        list: Список словарей с данными о финансовых транзакциях. Если файл пустой, содержит не список или не найден, возвращает пустой список.
+    """
+
+    # Определяем путь к файлу относительно текущего скрипта
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    project_root = os.path.join(current_dir, '..')
+    file_path = os.path.join(project_root, 'data', 'operations.json')
+
+    # Проверяем, существует ли файл и имеет ли он расширение .json
+    if not os.path.exists(file_path) or not file_path.endswith('.json'):
+        return []
+    else:
+        with open(file_path) as f:
+            data = json.load(f)
+        return data
+
+    # В корне проекта или из любой другой части проекта
+operations = read_transactions_from_json()
+print(operations)
